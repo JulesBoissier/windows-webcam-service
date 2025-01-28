4fed4fc21b6dc41be478daf285f984850a36a242
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-01-28 19:23:04 +0000

    Changed localhost ip to allow wsl to reach in

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ def video_feed():
     return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
